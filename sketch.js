a31fdbbfed05a6fb0c00f9795ce960be29dfa0b2
carlnelson19 <carlnelson19@gmail.com>
2017-02-13 13:38:16 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,76 @@
 function setup() {
+    createCanvas(500, 510);
+
 
 }
 
 function draw() {
+    background(73, 73, 73);
+    fill(150, 148, 148);
+    stroke(150, 148, 148);
+    rect(109, 78, 290, 315);
+    fill(178, 0, 0);
+    stroke(178, 0, 0);
+    rect(133, 325, 238, 40);
+    noFill();
+    stroke(255);
+    strokeWeight(30);
+    ellipse(145, 200, 20, 20);
+    ellipse(365, 200, 20, 20);
+   
+    fill(209, 180, 35);
+    ellipse(145, 200, 15, 15);
+    ellipse(365, 200, 15, 15);
+    
+    fill(0);
+    ellipse(145, 200, 5, 5);
+    ellipse(365, 200, 5, 5);
+    
+    fill(150, 148, 148);
+    stroke(150, 148, 148);
+    rect(130, 155, 25, 25);
+    rect(355, 155, 25, 25);
+    
+    fill(73, 73, 73);
+    stroke(73, 73, 73);
+    rect(154, 60, 200, 50);
+    
+    fill(150, 148, 148);
+    stroke(150, 148, 148);
+    rect(68, 410, 375, 85);
+    
+    stroke(255);
+    strokeWeight(5);
+    fill(255);
+    rect(140, 325, 18, 18);
+    rect(170, 325, 18, 18);
+    rect(205, 325, 18, 18);
+    rect(240, 325, 18, 18);
+    rect(310, 325, 18, 18);
+    rect(345, 325, 18, 18);
+    stroke(0);
+    triangle(250, 250, 250, 500);
+    triangle(250, 0, 250, 510);
+    fill(0);
+    rect(232, 185, 35, 70);
+    strokeWeight(0);
+    fill(150, 148, 148);
+    ellipse(250, 212, 17, 17);
+    rect(246, 218, 8, 20);
+    strokeWeight(5);
+    
+  
+    triangle(210, 0, 210, 510);
+    triangle(210, 0, 210, 510);
+    triangle(170, 0, 170, 510);
+    triangle(170, 0, 170, 510);
+    triangle(130, 0, 130, 510);
+    triangle(130, 0, 130, 510);
+    triangle(90, 0, 90, 510);
+    triangle(50, 0, 50, 510);
+    triangle(10, 0, 10, 510);
+    
+ 
+
 
 }
